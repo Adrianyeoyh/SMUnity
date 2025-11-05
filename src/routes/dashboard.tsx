39384557ce9517de497f1d4d98cc82810d98dbef
nnylac <calynnongyx@gmail.com>
2025-11-06 01:09:30 +0800

    added card

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -23,7 +23,7 @@ import {
   TrendingUp,
   Award,
   Heart,
-  CheckCircle2,
+  CircleCheckBig,
   AlertCircle,
   CircleCheck,
   ArrowRight,
@@ -81,6 +81,7 @@ function Dashboard() {
   const userName = userData?.name ?? "Student";
   // console.log("user data: ", userData);
 
+  console.log(userData);
   const [showCSUCard, setShowCSUCard] = useState(true);
   const [viewDialogOpen, setViewDialogOpen] = useState(false);
   const [selectedApplicationData, setSelectedApplicationData] = useState<any | null>(null);
@@ -183,7 +184,7 @@ function Dashboard() {
         )}
 
         {/* Stats Overview */}
-        <div className="grid grid-cols-1wait  lg:grid-cols-3 gap-6 mb-8">
+        <div className="grid grid-cols-1wait  lg:grid-cols-4 gap-6 mb-8">
 
           <Card>
             <CardHeader className="pb-4">
@@ -216,6 +217,23 @@ function Dashboard() {
             </CardHeader>
             <CardContent className="pt-0 pb-0">
               <div className="text-xs text-muted-foreground font-body">Awaiting response</div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="pb-4">
+              <div className="flex items-start justify-between">
+                <div className="flex-1">
+                  <CardDescription className="font-body mb-4 font-semibold">Successful Applications</CardDescription>
+                  <CardTitle className="font-heading text-3xl text-primary">{completedCount}</CardTitle>
+                </div>
+                <div className="hidden sm:block bg-blue-100 rounded-full p-3 ml-4">
+                  <CircleCheckBig className="h-6 w-6 text-blue-600" />
+                </div>
+              </div>
+            </CardHeader>
+            <CardContent className="pt-0 pb-0">
+              <div className="text-xs text-muted-foreground font-body">Total contributions</div>
             </CardContent>
           </Card>
 
